eb2eb7e15eccdc00d6ed3c07d39d1e2ae65d961b
Jai Sawhney <jai.sawhney1@gmail.com>
2022-02-16 15:34:13 -0800

    Add Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from db import db
+import models
 import os
 
 app = Flask(__name__)
-app.secret_key = os.environ.get("SECRET_KEY", os.urandom(6))
+
+app.config.update(
+    SECRET_KEY=os.environ.get("SECRET_KEY", os.urandom(6)),
+    SQLALCHEMY_DATABASE_URI=os.getenv("DATABASE_URL", "sqlite:///database.db"),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+)
+
+
+@app.before_first_request
+def create_db():
+    db.create_all()
 
 
 @app.route("/")
@@ -11,4 +23,5 @@ def index():
 
 
 if __name__ == "__main__":
+    db.init_app(app)
     app.run(host="0.0.0.0", port=os.environ.get("PORT", 3000))
---
+++ models.py
@@ -0,0 +1,8 @@
+from flask_login import UserMixin
+from db import db
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
